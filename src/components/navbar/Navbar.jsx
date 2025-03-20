d1f04b8fce7f86f1b514e6e992e40e640d64b99c
SiGu <miskybox@gmail.com>
2025-03-20 14:13:36 +0100

    Create: AuthForm component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,35 @@
-// src/components/Navbar.jsx
-import React from 'react';
-import styles from './Navbar.module.css';
+//Navbar.jsx
+import React, { useState } from "react";
+import LoginForm from "../login/AuthForm";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+    const [showLogin, setShowLogin] = useState(false);
+
     return (
-        <nav className={styles.navbar}>
-            <div className={styles.logo}>
-                <img src="/logo.png" alt="ArtPrint" />
-            </div>
-            <div className={styles.categories}>
-                <ul className={styles.categoryList}>
-                    <li><a href="#ceramica">Cerámica</a></li>
-                    <li><a href="#escritura">Escritura</a></li>
-                    <li><a href="#acuarela">Acuarela</a></li>
-                    <li><a href="#cocina">Cocina</a></li>
-                    <li><a href="#bordado">Bordado</a></li>
-                    <li><a href="#cosmetica">Cosmética</a></li>
-                </ul>
-            </div>
-            <div className={styles.navActions}>
-                <button className={styles.loginButton}>Iniciar sesión</button>
-                <span className={styles.cartIcon}>🛒</span>
-            </div>
-        </nav>
+        <>
+            <nav className={styles.navbar}>
+                <div className={styles.logo}>
+                    <img src="/logo.png" alt="ArtPrint" />
+                </div>
+                <div className={styles.categories}>
+                    <ul className={styles.categoryList}>
+                        <li><a href="#ceramica">Cerámica</a></li>
+                        <li><a href="#escritura">Escritura</a></li>
+                        <li><a href="#acuarela">Acuarela</a></li>
+                        <li><a href="#cocina">Cocina</a></li>
+                        <li><a href="#bordado">Fotografía</a></li>
+                        <li><a href="#cosmetica">Cosmética</a></li>
+                    </ul>
+                </div>
+                <div className={styles.navActions}>
+                    <button className={styles.loginButton} onClick={() => setShowLogin(true)}>Iniciar sesión</button>
+                    <span className={styles.cartIcon}>🛒</span>
+                </div>
+            </nav>
+
+            {showLogin && <LoginForm />}
+        </>
     );
 };
 
